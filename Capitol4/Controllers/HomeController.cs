5f5a9dd72a90e7b88eb1ce306ccdb97359b9dcf2
Liviu Mateescu <liviumateescu@hotmail.com>
2017-11-19 23:51:56 +0200

    c

---
+++ Capitol4/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Capitol4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View(new string[] {"C#", "Language", "Features" });
+        }
+    }
+}
