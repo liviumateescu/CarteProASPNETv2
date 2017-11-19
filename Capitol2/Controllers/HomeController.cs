df750c1d0c882dd6042354a72e84f381c2aa4b80
Liviu Mateescu <liviumateescu@hotmail.com>
2017-11-19 21:38:30 +0200

    a

--- Capitol2/Controllers/HomeController.cs
+++ Capitol2/Controllers/HomeController.cs
@@ -1,16 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Capitol2.Models;
+using System;
 
 namespace Capitol2.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView");
+        }
+
+        public ViewResult RsvpForm()
         {
             return View();
         }
